2a47134d9079e0c5225d14147e15d589796a84e9
Wansuko-cmd <63241922+Wansuko-cmd@users.noreply.github.com>
2021-10-24 15:31:55 +0900

    [add] 他のメソッドを追加

--- main.py
+++ main.py
@@ -5,19 +5,35 @@
 import requests
 
 
+url = 'https://ktor-chat-app.herokuapp.com/chat'
+
+
 def get_messages():
-    response = requests.get('https://ktor-chat-app.herokuapp.com/chat')
+    response = requests.get(url)
     print(response.status_code)
     print(response.text)
 
 
-def create_messages(user_names, texts):
-    response = requests.post('https://ktor-chat-app.herokuapp.com/chat', json={'user_name': user_names, 'text': texts})
+def create_messages(user_name: str, text: str):
+    response = requests.post(url, json={'user_name': user_name, 'text': text})
+    print(response.status_code)
+    print(response.text)
+
+
+def update_message(message_id: str, user_name: str, text: str):
+    response = requests.put(url, json={'id': message_id, 'user_name': user_name, 'text': text})
+    print(response.status_code)
+    print(response.text)
+
+
+def delete_message(message_id: str):
+    response = requests.delete(f'{url}/{message_id}')
     print(response.status_code)
     print(response.text)
 
 
 # Press the green button in the gutter to run the script.
+# def main():
 if __name__ == '__main__':
 
     while True:
@@ -30,6 +46,16 @@ if __name__ == '__main__':
             user_name = input('user_name: ')
             text = input('text: ')
             create_messages(user_name, text)
+
+        elif command == 'update':
+            message_id = input('id: ')
+            user_name = input('user_name: ')
+            text = input('text: ')
+            update_message(message_id, user_name, text)
+
+        elif command == 'delete':
+            message_id = input('id: ')
+            delete_message(message_id)
 
         elif command == 'exit':
             break
